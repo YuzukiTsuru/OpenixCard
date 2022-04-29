91194d075b277dff88fa73e69d1379c1fa0be25f
YuzukiTsuru <GloomyGhost@GloomyGhost.com>
2022-04-29 14:42:06 +0800

    add cpack for package

---
+++ CMake/deb.cmake
@@ -0,0 +1,12 @@
+set(CPACK_GENERATOR "DEB" "TGZ" "STGZ" "ZIP")
+
+set(CPACK_PACKAGE_VENDOR "YuzukiTsuru")
+set(CPACK_PACKAGE_DESCRIPTION "Open Source Version of Allwinner PhoenixCard to Dump, Unpack, Flash Allwinner IMG Files")
+
+set(CPACK_DEBIAN_PACKAGE_NAME "OpenixCard")
+set(CPACK_DEBIAN_PACKAGE_MAINTAINER "YuzukiTsuru")
+set(CPACK_DEBIAN_PACKAGE_DESCRIPTION "Open Source Version of Allwinner PhoenixCard to Dump, Unpack, Flash Allwinner IMG Files")
+set(CPACK_DEBIAN_PACKAGE_VERSION "1.0.${PACKAGE_VERSION}")
+set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
+set(CPACK_DEBIAN_PACKAGE_DEPENDS "libconfuse")
+set(CPACK_DEBIAN_PACKAGE_ARCHITECTURE "x86_64")
